adc6f78fe8106861f1bfef149d830e38bb4a38fa
Zack <zackvalis@gmail.com>
2023-11-16 17:55:01 -0500

    Continue styling & Begin footer

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,17 @@
 //import * as THREE from "three";
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import {
   Center,
   View,
   Preload,
   Stats,
+  Resize,
   OrbitControls,
   PresentationControls,
   Environment, } from '@react-three/drei';
 import { useControls, Leva } from 'leva';
+import moment from 'moment-timezone';
 import CameraPositionLogger from './three/helpers/CameraPositionLogger';
 import Text from './three/DreiText2';
 import HDR from '../static/textures/venice_sunset_1k.hdr';
@@ -20,11 +22,32 @@ const Home = () => {
   const infoView = useRef();
   const workView = useRef();
 
-
   const { concurrent, distributed } = useControls({
     concurrent: { value: true },
     distributed: { value: true },
   });
+
+  const [estTime, setEstTime] = useState('');
+
+  useEffect(() => {
+    // Function to update EST time
+    function updateESTTime() {
+      // Get current time in Eastern Time Zone (EST)
+      const estTime = moment.tz('America/New_York').format('HH:mm:ss');
+
+      // Update state with formatted EST time
+      setEstTime(estTime);
+    }
+
+    // Call the function to update EST time when the component mounts
+    updateESTTime();
+
+    // Update EST time every second (optional)
+    const intervalId = setInterval(updateESTTime, 1000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures that this effect runs once after the initial render
 
   return (
     <div ref={ref} className='home'>
@@ -34,36 +57,53 @@ const Home = () => {
             <span>ZACHERIAH <br /> VALIS</span>
           </div>
           <div className='folio'>
-            <span>SOFTWARE ENGINEER <br /> FOLIO / 2023</span>
+            <span>SOFTWARE ENGINEER <br /> PORTFOLIO / 2023</span>
           </div>
         </div>
-
-        <div className='home__header__center'ref={nameView}  />
 
         <div className='home__header__right'>
           <div className='availability'>
             <span>AVAILABLE FOR FREELANCE <br /> WORK FROM JANUARY 2024</span>
           </div>
           <div className='contact'>
-            <span>Contact</span>
+            <span>CONTACT</span>
           </div>
         </div>
       </section>
 
       <section className="home__nav">
-        <div ref={infoView} className="left">
+        <div className="left" ref={infoView} />
+        <div className='center' ref={nameView} />
+        <div className="right" ref={workView} />
 
-        </div>
-        <div ref={workView} className="right">
-
-        </div>
       </section>
 
       <section className='home__hero'>
+        <div className='text'>
+          <h1>"PLACEHOLDER"</h1>
+        </div>
       </section>
 
       <section className='home__footer'>
+        <div className='footer__left'>
+          <p className='place'>CLT, NC</p>
+          <div className='time'>{estTime}</div>
+        </div>
+        <div className='footer__center'>
+          <div className='socials'>
+            <span>SPOTIFY</span>
+            <span>INSTAGRAM</span>
+            <span>GITHUB</span>
+            <span>LINKEDIN</span>
+          </div>
+        </div>
+        <div className='footer__right'>
+          <div className='credit'>
+            <span>ACKNOWLEDGEMENTS</span>
+          </div>
+        </div>
       </section>
+
     <Leva hidden />
     <Canvas
       eventSource={ref}
@@ -71,59 +111,66 @@ const Home = () => {
       key={concurrent}
       mode={concurrent ? "concurrent" : "blocking"}
       shadows
-      camera={{ position: [-3, 0.75, 6], zoom: 1, fov: 30 }}
+      camera={{ position: [-3, 0.75, 5], zoom: 1, fov: 30 }}
       gl={{ preserveDrawingBuffer: true }}
       >
       <Suspense fallback={null}>
         <View index={1} track={nameView}>
-          <Center top>
+          <Center>
           <Environment files={HDR} background={false}/>
           <PresentationControls
+            enabled={true}
+            rotation={[0,0,0]}
             config={{ mass: 2, tension: 500 }}
             snap={{ mass: 4, tension: 1500 }}
             polar={[-Math.PI / 3, Math.PI / 3]}
             azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-              <Text size={10}>Zack Valis</Text>
-            </PresentationControls>
-
+              <Resize scale={8}>
+                <Text>Zack Valis</Text>
+              </Resize>
+          </PresentationControls>
           </Center>
         </View>
         <View index={2} track={infoView}>
           <Environment files={HDR} background={false}/>
           <PresentationControls
+            enabled={true}
+            rotation={[0,0,0]}
             config={{ mass: 2, tension: 500 }}
             snap={{ mass: 4, tension: 1500 }}
             polar={[-Math.PI / 3, Math.PI / 3]}
             azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-              <Text size={10}>Info</Text>
-            </PresentationControls>
+              <Text>Info</Text>
+          </PresentationControls>
         </View>
         <View index={3} track={workView}>
           <Environment files={HDR} background={false}/>
           <PresentationControls
+            enabled={true}
+            rotation={[0,0,0]}
             config={{ mass: 2, tension: 500 }}
             snap={{ mass: 4, tension: 1500 }}
             polar={[-Math.PI / 3, Math.PI / 3]}
             azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-              <Text size={10}>Work</Text>
-            </PresentationControls>
+              <Text>Work</Text>
+          </PresentationControls>
         </View>
       </Suspense>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <OrbitControls
-        enabled={false}
-        autoRotateSpeed={-0.1}
-        zoomSpeed={0.25}
-        minZoom={20}
-        maxZoom={100}
-        enablePan={false}
-        dampingFactor={0.05}
-        minPolarAngle={-Math.PI / 2}
-        maxPolarAngle={(0.99 * Math.PI) / 2}
-      />
       <CameraPositionLogger event='mousedown' />
       <Preload all />
+      <OrbitControls
+            enabled={false}
+            autoRotateSpeed={-0.1}
+            zoomSpeed={0.25}
+            minZoom={20}
+            maxZoom={100}
+            enablePan={false}
+            dampingFactor={0.05}
+            minPolarAngle={-Math.PI / 2}
+            maxPolarAngle={(0.99 * Math.PI) / 2}
+          />
     </Canvas>
   </div>
   );
