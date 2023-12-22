4070cd5e4f3fc4a029a58d3d237594a850ffd919
Zack <zackvalis@gmail.com>
2023-12-21 19:46:12 -0500

    Begin Info Page Content

--- frontend/src/components/Credit.js
+++ frontend/src/components/Credit.js
@@ -7,17 +7,19 @@ const Credit = () => {
         <div className='credit__text'>
           <h1>"ACKNOWLEDGEMENTS"</h1>
           <h3>Modern coding requires taking bits of
-            code from a miriad of sources and molding it
+            code from a miriad of sources, learning from it, and molding it
             into the program you need in order to achieve your vision.<br />
             This page is dedicated to all the developers from which I got
             inspiration and code from.<br />
             Thank you!
           </h3>
-          <a className="link" href="https://www.seyi.dev/">Oluwadare Oluwaseyi [Stylistic Inspiration + Code]</a>
+          <a className="link" href="https://www.seyi.dev/">Oluwadare Oluwaseyi [Stylistic Inspiration + SCSS Code]</a>
           <br />
           <a className="link" href="https://www.fayemi.design/">Isaac Fayemi [Stylistic Inspiration]</a>
           <br />
           <a className="link" href="https://www.github.com/akella">Yuri Artiukh AKA "Akella" [3D Text Code]</a>
+          <br />
+          <a className="link" href="https://youtu.be/1Hg6gQs4RDQ?si=HN8MF0tKU9uMr9jJ">ABSURD 3D YouTube Channel [Camera Position Logger Code]</a>
         </div>
       </section>
     </div>
--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -2,10 +2,38 @@
 const Info = () => {
 
   return (
-    <div className='home'>
+    <div className='hero'>
       <section className='container'>
-        <div className='text'>
-          <h1>"INFO PAGE"</h1>
+        <div className='info'>
+          <h1 className='text'>"ABOUT ZACK VALIS"</h1>
+          <p className='text'>My name is Zack and I am a skilled
+          Software Developer currently based out of Charlotte,
+          North Carolina. I specialize in Data Science and Web
+          Development, but I am always willing and eager to learn
+          more in order to grow my skills.
+          I graduated from USC Upstate in 2020
+          with a BS in Computer Science and went back to school
+          in 2022 to earn a certificate in Data Analytics from
+          UNC Charlotte.
+          I'm a well rounded person who loves the
+          intersection between form and function.
+          When developing this website, I tirelessly blended the
+          two and took great inspiration from hip hop
+          and streetwear culture in order to make the vision I had
+          for my website a reality.
+          </p>
+          <p className='text'>Some of my personal interests: I'm a big sports guy who likes to
+          watch football, basketball, and Formula 1. I also have a
+          couple cars that I like to work on, maintain, and customize.
+          I am currently rebuilding my 2012 Subaru STi hatch to make
+          roughly 400whp and plan one day to make my 1990 Miata into
+          a WTAC compliant racecar. I also like to dabble in fashion,
+          primarily wearing streewear brands and have a small, but
+          growing sneaker collection. In 2017/2018, while studying
+          abroad in Germany, I started to get into photography and
+          continue to play around with different cameras and angles
+          only using natural lighting with little to no
+          post-processing.</p>
         </div>
       </section>
     </div>
