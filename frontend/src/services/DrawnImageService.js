130d5549e5c155f77d642469131905824db55e21
Zack <zackvalis@gmail.com>
2024-01-15 00:44:46 -0500

    Import More Images

--- frontend/src/services/DrawnImageService.js
+++ frontend/src/services/DrawnImageService.js
@@ -5,10 +5,24 @@ import img4 from '../static/drawn_images/redline.gif';
 import img5 from '../static/CIMG0894.JPG';
 import img6 from '../static/CIMG1165.JPG';
 import img7 from '../static/logo.png';
+import img8 from '../static/drawn_images/CIMG0861.JPG';
+import img9 from '../static/drawn_images/CIMG0888.JPG';
+import img10 from '../static/drawn_images/CIMG0979.JPG';
+import img11 from '../static/drawn_images/CIMG1001.JPG';
+import img12 from '../static/drawn_images/CIMG1066.JPG';
+import img13 from '../static/drawn_images/CIMG1124.JPG';
+import img14 from '../static/drawn_images/CIMG1147.JPG';
+import img15 from '../static/drawn_images/CIMG1154.JPG';
+import img16 from '../static/drawn_images/CIMG1169.JPG';
+import img17 from '../static/drawn_images/CIMG1190.JPG';
+import img18 from '../static/drawn_images/zvalis_webdev.png';
+import img19 from '../static/drawn_images/cm1.jpg';
+import img20 from '../static/drawn_images/me1.jpg';
 
 export function ImageArray() {
-  const array = [img1, img5, img3,
-                 img6, img4, img7,
-                 img2,]
+  const array = [img18, img19, img20, img8, img1,
+                  img9, img17, img10, img16, img2,
+                  img11, img5, img12, img14, img3,
+                  img13, img15, img6, img4, img7,]
   return (array)
 };
