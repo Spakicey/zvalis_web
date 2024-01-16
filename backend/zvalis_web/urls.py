1e5b48e5fb5d8e674b38bfeaee789e2daac7fe2e
Zack <zackvalis@gmail.com>
2024-01-16 11:47:17 -0500

    Update url patterns

--- backend/zvalis_web/urls.py
+++ backend/zvalis_web/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('projectapi/', include('apps/projects.urls')),
+    path('', TemplateView.as_view(template_name='backend/index.html'))
+]
