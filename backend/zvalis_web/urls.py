7811fc28024d00b1815951721426ff7bb9337f93
Zack <zackvalis@gmail.com>
2024-01-16 12:03:19 -0500

    Update project url path

--- backend/zvalis_web/urls.py
+++ backend/zvalis_web/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projectapi/', include('apps/projects.urls')),
+    path('projectapi/', include('projects.urls')),
     path('', TemplateView.as_view(template_name='backend/index.html'))
 ]
