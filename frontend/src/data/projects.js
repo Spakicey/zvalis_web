93c573b57324ce7b58d09a965c6aed68f4c8f44b
Zack <zackvalis@gmail.com>
2024-01-21 11:42:47 -0500

    Switch to regular json for data

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,13 +1,13 @@
 // projectData.js
 
-export const projectData = [
+export const projectJSON = [
   {
     id: 0,
     name: '2022 Formula 1 Analysis',
     summary: 'Mid-season analysis to draw predictions',
     description: 'Scraping half of the 2022 Formula 1 season data off their website, cleaning the data, loading into MongoDB, and displaying the data through a Flask webapp. Then fitting the data to a LightGBM model for season predictions',
     technology: 'Python, Juptyer, Flask, MongoDB, LightGBM',
-    image: require('../static/project_images/f1_analysis.jpg'),
+    image: '',
     repository: 'https://github.com/Spakicey/2022-Formula1-Analysis',
     deployment: '',
     visible: 'visible',
@@ -18,7 +18,7 @@ export const projectData = [
     summary: 'Automotive statistical analysis',
     description: 'Using R to gain new insights into mpg and suspension coil PSI data',
     technology: 'R, Linear Regression, T-Test',
-    image: require('../static/project_images/mechaCar.png'),
+    image: '',
     repository: 'https://github.com/Spakicey/mechacar-statistical-analysis',
     deployment: '',
     visible: 'visible',
@@ -29,7 +29,7 @@ export const projectData = [
     summary: 'Interactive maps',
     description: 'Creating interactive maps displaying tectonic plates and earthquakes locations and magnitudes',
     technology: 'D3, GeoJSON, JavaScript',
-    image: require('../static/project_images/EQ_IMG.png'),
+    image: '',
     repository: 'https://github.com/Spakicey/mapping-earthquakes',
     deployment: '',
     visible: 'visible',
@@ -40,7 +40,7 @@ export const projectData = [
     summary: 'Interactive dashboard for volunteers\' data',
     description: 'This project contains interactive charts for top 10 bacterial species (OTUs), a bubble chart showing bacteria cultures per sample, and a guage chart showing weekly frequency of belly button washes',
     technology: 'JavaScript, D3, Plotly',
-    image: require('../static/project_images/bellyButton.png'),
+    image: '',
     repository: 'https://github.com/Spakicey/belly-button-biodiversity',
     deployment: 'https://spakicey.github.io/belly-button-biodiversity/',
     visible: 'visible',
@@ -51,7 +51,7 @@ export const projectData = [
     summary: '',
     description: 'As I begin freelancing, I should have more works to add!',
     technology: 'Brain Power',
-    image: require('../static/drawn_images/CIMG0861.JPG'),
+    image: '',
     repository: 'https://github.com/Spakicey',
     deployment: '',
     visible: 'visible',
